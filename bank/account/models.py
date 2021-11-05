9496a16b3a98f38e7f717e6ea22a17a38eac28e3
yeet404 <93748878+yeet404@users.noreply.github.com>
2021-11-05 19:47:17 -0400

    Create History model

--- bank/account/models.py
+++ bank/account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class History(models.Model):
+    transaction_type = models.CharField(max_length=8)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.transaction_type}: ${self.amount}"
