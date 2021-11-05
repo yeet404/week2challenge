97c91bc480f5dd6632b3a434adcb74f66cb4ee84
yeet404 <93748878+yeet404@users.noreply.github.com>
2021-11-05 12:44:43 -0400

    Create bal page

--- bank/home/views.py
+++ bank/home/views.py
@@ -1,8 +1,26 @@
-from django.http.response import HttpResponseRedirect
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+
+history = [
+    ("withdraw", 2345),
+    ("desposit", 234),
+    ("withdraw", 2345),
+    ("desposit", 234),
+    ("withdraw", 2345),
+    ("desposit", 234),
+    ("withdraw", 2345),
+    ("desposit", 234),
+    ("withdraw", 2345),
+    ("desposit", 234)
+]
 
 # Create your views here.
 def index(request):
     return render(request, "home/index.html", {
         "name": "Yeet Fourzerofour"
+    })
+
+def bal(request):
+    return render(request, "home/bal.html", {
+        "history": history
     })
