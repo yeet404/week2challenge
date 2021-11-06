ad2f3eb6e2902d113afaabecda9f4222b5925ed3
yeet404 <93748878+yeet404@users.noreply.github.com>
2021-11-06 18:44:32 -0400

    Finish backend for register

--- bank/accounts/urls.py
+++ bank/accounts/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = "accounts"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("register", views.register, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout")
 ]
