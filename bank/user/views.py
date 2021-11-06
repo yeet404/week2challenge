a267b1206ea01dce889f73c0e9b47f4c6bf40545
yeet404 <93748878+yeet404@users.noreply.github.com>
2021-11-06 17:29:07 -0400

    Create Transaction obj in view

--- bank/user/views.py
+++ bank/user/views.py
@@ -1,34 +1,24 @@
+from django.db.models import Sum
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from .models import Transaction
 
-history = [
-    ("withdraw", 2345),
-    ("desposit", 234),
-    ("withdraw", 2345),
-    ("desposit", 234),
-    ("withdraw", 2345),
-    ("desposit", 234),
-    ("withdraw", 2345),
-    ("desposit", 234),
-    ("withdraw", 2345),
-    ("desposit", 234)
-]
-
 # Create your views here.
 def index(request):
     if request.user.is_authenticated:
+        user_transactions = Transaction.objects.filter(user=request.user)
+        balance = sum(user_transactions.values_list('amount', flat=True))
         return render(request, "user/index.html", {
-            "name": "Yeet Fourzerofour",
-            "transactions": Transaction.objects.all() 
+            "transactions": user_transactions,
+            "balance": balance
         })
     return HttpResponseRedirect(reverse("accounts:login"))
 
 def transaction(request):
     if request.method == "POST":
-        transaction = (request.POST["transaction-type"], int(request.POST["money"]))
-        history.append(transaction)
+        t = Transaction(transaction_type=request.POST["transaction-type"], amount=request.POST["money"], user=request.user)
+        t.save()
         return render(request, "user/transaction.html", {
             "message": "Transaction completed successfully"
         })
