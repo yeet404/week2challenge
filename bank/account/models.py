b834bb55c0bfdf5d95cc8db7c7de897c3373ad88
yeet404 <93748878+yeet404@users.noreply.github.com>
2021-11-05 20:38:49 -0400

    Link User to Transaction

--- bank/account/models.py
+++ bank/account/models.py
@@ -1,17 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Username(models.Model):
-    username = models.CharField(max_length=64)
-
-    def __str__(self):
-        return f"{self.username}"
-
-
 class Transaction(models.Model):
     transaction_type = models.CharField(max_length=8)
     amount = models.IntegerField()
-    user = models.ForeignKey(Username, on_delete=models.CASCADE, related_name="transaction_maker")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="transaction_maker", null=True)
 
     def __str__(self):
-        return f"{self.transaction_type}: ${self.amount}"
+        return f"({self.user}) {self.transaction_type}: ${self.amount}"
