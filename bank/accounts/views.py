516b3794d2b341eefc466e54cfeddd3d112366d4
yeet404 <93748878+yeet404@users.noreply.github.com>
2021-11-06 18:39:26 -0400

    Finish register backend

--- bank/accounts/views.py
+++ bank/accounts/views.py
@@ -1,15 +1,30 @@
 from django.contrib import auth
 from django.contrib.auth import authenticate, login, logout
 from django.http.response import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("accounts:login"))
     return redirect("/user/")
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect("user:index")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    
+    form = UserCreationForm()
+    return render (request=request, template_name="accounts/register.html", context={"form":form})
 
 def login_view(request):
     if request.method == "POST":
